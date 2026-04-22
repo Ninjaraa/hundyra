7d6527243dc25e04f4c67580580e557c8b59d026
Ninjaraa <ninja.claesson@gmail.com>
2026-04-22 20:28:56 +0200

    change email address

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmailService {
-  emailAddress = 'info@hundyra.se';
+  emailAddress = 'hundyra@gmail.com';
 }
